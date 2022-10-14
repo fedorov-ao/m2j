9a5b4d0772062436e916c601ee16bf2c13cd754f
fedorov-ao <fedorov-ao@yandex.ru>
2022-10-14 18:27:00 +0300

    Added bindsNextLevel parameter.

--- m2j.py
+++ m2j.py
@@ -5751,9 +5751,9 @@ def make_parser():
         sink.set_next(next)
     def add(next, sink):
       if next is not None:
-        #Next sink is added to level 0 so it will be able to process events that were processed by other binds.
+        #By default next sink is added to level 0 so it will be able to process events that were processed by other binds.
         #This is useful in case like when a bind and a mode both need to process some axis event.
-        sink.add(None, next, 0)
+        sink.add(None, next, get_nested_d(cfg, "bindsNextLevel", 0))
     try:
       #TODO Refactor
       if len(cfg) == 0:
