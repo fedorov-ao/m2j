b4dd8a84483d1ef6f2582df6bcdb73ab6e74a1c1
fedorov-ao <fedorov-ao@yandex.ru>
2023-09-06 21:43:18 +0300

    Refactored DirectInputKeyboard.

--- m2j_win.py
+++ m2j_win.py
@@ -608,16 +608,23 @@ VK_PA1 = 0xFD
 VK_OEM_CLEAR = 0xFE
 
 
-#TODO Implement
 g_vkey2codeDict = {
   VK_PLAY : codes.KEY_PLAY, VK_F21 : codes.KEY_F21, VK_PAUSE : codes.KEY_PAUSE, VK_HOME : codes.KEY_HOME, VK_HELP : codes.KEY_HELP, VK_HANGEUL : codes.KEY_HANGEUL, VK_SELECT : codes.KEY_SELECT, VK_RIGHT : codes.KEY_RIGHT, VK_HANJA : codes.KEY_HANJA, VK_TAB : codes.KEY_TAB, VK_F9 : codes.KEY_F9, VK_F8 : codes.KEY_F8, VK_F3 : codes.KEY_F3, VK_F2 : codes.KEY_F2, VK_F1 : codes.KEY_F1, VK_F7 : codes.KEY_F7, VK_F6 : codes.KEY_F6, VK_F5 : codes.KEY_F5, VK_F4 : codes.KEY_F4, VK_F22 : codes.KEY_F22, VK_F23 : codes.KEY_F23, VK_ZOOM : codes.KEY_ZOOM, VK_CANCEL : codes.KEY_CANCEL, VK_DELETE : codes.KEY_DELETE, VK_SPACE : codes.KEY_SPACE, VK_F24 : codes.KEY_F24, VK_NUMLOCK : codes.KEY_NUMLOCK, VK_END : codes.KEY_END, VK_PRINT : codes.KEY_PRINT, VK_CLEAR : codes.KEY_CLEAR, VK_MENU : codes.KEY_MENU, VK_LEFT : codes.KEY_LEFT, VK_BACK : codes.KEY_BACK, VK_INSERT : codes.KEY_INSERT, VK_UP : codes.KEY_UP, VK_NEXT : codes.KEY_NEXT, VK_F10 : codes.KEY_F10, VK_F20 : codes.KEY_F20, VK_SLEEP : codes.KEY_SLEEP, VK_F19 : codes.KEY_F19, VK_F18 : codes.KEY_F18, VK_F13 : codes.KEY_F13, VK_F12 : codes.KEY_F12, VK_F11 : codes.KEY_F11, VK_DOWN : codes.KEY_DOWN, VK_F17 : codes.KEY_F17, VK_F16 : codes.KEY_F16, VK_F15 : codes.KEY_F15, VK_F14 : codes.KEY_F14, VK_SCROLL : codes.KEY_SCROLLLOCK,
   VK_SHIFT : codes.KEY_RIGHTSHIFT, VK_CONTROL : codes.KEY_RIGHTCTRL, VK_MENU : codes.KEY_RIGHTALT,
   VK_APPS : codes.KEY_APPSELECT,
   0x41 : codes.KEY_A, 0x42 : codes.KEY_B, 0x43 : codes.KEY_C, 0x44 : codes.KEY_D, 0x45 : codes.KEY_E, 0x46 : codes.KEY_F, 0x47 : codes.KEY_G, 0x48 : codes.KEY_H, 0x49 : codes.KEY_I, 0x4a : codes.KEY_J, 0x4b : codes.KEY_K, 0x4c : codes.KEY_L, 0x4d : codes.KEY_M, 0x4e : codes.KEY_N, 0x4f : codes.KEY_O, 0x50 : codes.KEY_P, 0x51 : codes.KEY_Q, 0x52 : codes.KEY_R, 0x53 : codes.KEY_S, 0x54 : codes.KEY_T, 0x55 : codes.KEY_U, 0x56 : codes.KEY_V, 0x57 : codes.KEY_W, 0x58 : codes.KEY_X, 0x59 : codes.KEY_Y, 0x5a : codes.KEY_Z, }
+
 def vkey2code(vkey):
   r = g_vkey2codeDict.get(vkey, None)
-  rStr = "0x{:x} ({})".format(r, typecode2name(codes.EV_KEY, r)) if r is not None else "None"
+  #rStr = "0x{:x} ({})".format(r, typecode2name(codes.EV_KEY, r)) if r is not None else "None"
   #logger.debug("vkey2code(): 0x{:x} -> {}".format(vkey, rStr))
+  return r
+
+
+g_code2vkeyDict = { code : vkey for vkey,code in g_vkey2codeDict.items() }
+
+def code2vkey(code):
+  r = g_code2vkeyDict.get(code, None)
   return r
 
 
@@ -874,15 +881,25 @@ class INPUT(Structure):
 
 
 class DirectInputKeyboard:
+  MODE_VK = 0
+  MODE_SCANCODE = 1
   def set_key_state(self, key, state):
     #logger.debug("{}: Setting key {} (0x{:X}) to {}".format(self, typecode2name(codes.EV_KEY, key), key, state))
     extra = ULONG(0)
     inpt = INPUT()
     inpt.type = INPUT_KEYBOARD
-    inpt.ki.wVk = 0
+    if self.mode_ == self.MODE_SCANCODE:
+      inpt.ki.wVk = 0
+    elif self.mode_ == self.MODE_VK:
+      vkey = code2vkey(key)
+      if vkey is None:
+        raise RuntimeError("Cannot convert key {} to VKode".format(key))
+      inpt.ki.wVk = vkey
+    else:
+      assert(False)
     #Key codes in ecodes and DirectInput match, so no conversion is needed
-    inpt.ki.wScan = key
-    inpt.ki.dwFlags = KEYEVENTF_SCANCODE | (KEYEVENTF_KEYUP if state == 0 else 0)
+    inpt.ki.wScan = key if self.mode_ == self.MODE_SCANCODE else 0
+    inpt.ki.dwFlags = (KEYEVENTF_SCANCODE if self.mode_ == self.MODE_SCANCODE else 0) | (KEYEVENTF_KEYUP if state == 0 else 0)
     inpt.ki.time = 0
     inpt.ki.dwExtraInfo = pointer(extra)
     if windll.user32.SendInput(1, byref(inpt), sizeof(inpt)) != 1:
@@ -891,9 +908,19 @@ class DirectInputKeyboard:
   def set_button_state(self, key, state):
     return self.set_key_state(key, state)
 
+  def __init__(self, mode=1):
+    self.mode_ = mode
+
 
 def parseDirectInputKeyboardOutput(cfg, state):
-  return DirectInputKeyboard()
+  mode = state.resolve_d(cfg, "mode", "SCANCODE")
+  if mode == "SCANCODE":
+    mode = DirectInputKeyboard.MODE_SCANCODE
+  elif mode == "VK":
+    mode = DirectInputKeyboard.MODE_VK
+  else:
+    raise RuntimeError("Bad mode: '{}'".format(mode))
+  return DirectInputKeyboard(mode)
 
 
 class WNDCLASS(Structure):
