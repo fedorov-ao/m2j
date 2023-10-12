3147499c4ef38426ee402f929c6c2f61bda18ea1
fedorov-ao <fedorov-ao@yandex.ru>
2023-10-12 11:02:46 +0300

    Refactored tests.

--- m2j_test.py
+++ m2j_test.py
@@ -8,102 +8,46 @@ from m2j import *
 #IterativeInputOp
 class IncIterativeInputOpTestCase(unittest.TestCase):
   longMessage = True
+  delta = 0.001
+  inputLimits = [-1.0, 1.0]
+  coeffs = {1:2.0}
+  eps = 0.001
+  numSteps = 100
+  values = (
+    (0.0, 0.0),
+    (1.0, 2.0),
+    (-1.0, -2.0)
+  )
 
   def setUp(self):
-    outputOp = ApproxOp(PolynomialApproximator(coeffs=[0.0, 2.0]))
-    self.inputOp = IterativeInputOp(outputOp=outputOp, eps=0.001, numSteps=100)
+    outputOp = FuncOp(PolynomialFunc(coeffs=self.coeffs))
+    self.inputOp = IterativeInputOp(outputOp=outputOp, eps=self.eps, numSteps=self.numSteps)
 
-  def test_0(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(0.0, inputLimits), 0.0, delta=delta)
-
-  def test_1(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(2.0, inputLimits), 1.0, delta=delta)
-
-  def test_minus1(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(-2.0, inputLimits), -1.0, delta=delta)
+  def test(self):
+    for x,y in self.values:
+      self.assertAlmostEqual(self.inputOp.calc(y, self.inputLimits), x, delta=self.delta)
 
 
 class DecIterativeInputOpTestCase(unittest.TestCase):
   longMessage = True
+  delta = 0.001
+  inputLimits = [-1.0, 1.0]
+  coeffs = {1:-2.0}
+  eps = 0.001
+  numSteps = 100
+  values = (
+    (0.0, 0.0),
+    (1.0, -2.0),
+    (-1.0, 2.0)
+  )
 
   def setUp(self):
-    outputOp = ApproxOp(PolynomialApproximator(coeffs=[0.0, -2.0]))
-    self.inputOp = IterativeInputOp(outputOp=outputOp, cmp=lambda a,b: a > b, eps=0.001, numSteps=100)
+    outputOp = FuncOp(PolynomialFunc(coeffs=self.coeffs))
+    self.inputOp = IterativeInputOp(outputOp=outputOp, cmp=lambda a,b: a > b, eps=self.eps, numSteps=self.numSteps)
 
-  def test_0(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(0.0, inputLimits), 0.0, delta=delta)
-
-  def test_1(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(2.0, inputLimits), -1.0, delta=delta)
-
-  def test_minus1(self):
-    delta = 0.001
-    inputLimits = [-1.0, 1.0]
-    self.assertAlmostEqual(self.inputOp.calc(-2.0, inputLimits), 1.0, delta=delta)
-
-
-#LookupOp
-def make_lookup_op(coeffs, inputStep, cmp=lambda a,b: a < b):
-  outputOp = ApproxOp(PolynomialApproximator(coeffs=coeffs))
-  inputOp = IterativeInputOp(outputOp=outputOp, cmp=cmp, eps=0.001, numSteps=100)
-  lookupOp = LookupOp(inputOp=inputOp, outputOp=outputOp, inputStep=inputStep)
-  return lookupOp
-
-
-class IncLookupOpTestCase(unittest.TestCase):
-  longMessage = True
-
-  def setUp(self):
-    self.lookupOp = make_lookup_op(coeffs=[0.0, 2.0], inputStep=0.1)
-
-  def test_init(self):
-    self.assertEqual(self.lookupOp.ivs_, [0.0, 0.1])
-    self.assertEqual(self.lookupOp.ovs_, [0.0, 0.2])
-
-  def test_0(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(0.0), 0.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
-
-  def test_1(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(2.0), 1.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
-
-  def test_minus1(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(-2.0), -1.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
-    
-
-class DecLookupOpTestCase(unittest.TestCase):
-  longMessage = True
-    
-  def setUp(self):
-    self.lookupOp = make_lookup_op(coeffs=[0.0, -2.0], inputStep=0.1, cmp=lambda a,b : a < b)
-
-  def test_init(self):
-    self.assertEqual(self.lookupOp.ivs_, [0.1, 0.0])
-    self.assertEqual(self.lookupOp.ovs_, [-0.2, 0.0])
-
-  def test_0(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(0.0), 0.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
-
-  def test_1(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(-2.0), 1.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
-
-  def test_minus1(self):
-    delta = 0.001
-    self.assertAlmostEqual(self.lookupOp.calc(2.0), -1.0, delta=delta, msg="ivs_: {}, ovs_: {}".format(self.lookupOp.ivs_, self.lookupOp.ovs_))
+  def test(self):
+    for x,y in self.values:
+      self.assertAlmostEqual(self.inputOp.calc(y, self.inputLimits), x, delta=self.delta)
 
 
 if __name__ == '__main__':
